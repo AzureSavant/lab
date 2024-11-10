78385e5a4dfb87b188e2680ecb529db34e3bd3f7
Aleksandar Nonevski <Aleksandar.Nonevski@students.finki.ukim.mk>
2024-11-11 00:16:23 +0100

    Add Id To Event Model, Add Location Relation

--- src/main/java/mk/finki/ukim/mk/lab/model/Event.java
+++ src/main/java/mk/finki/ukim/mk/lab/model/Event.java
@@ -2,13 +2,27 @@ package mk.finki.ukim.mk.lab.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Event {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
     protected String name;
     protected String description;
     protected double popularityScore;
+    @OneToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 
+    public Event(String name, String description, double popularityScore) {
+        this.name = name;
+        this.description = description;
+        this.popularityScore = popularityScore;
+    }
 }
