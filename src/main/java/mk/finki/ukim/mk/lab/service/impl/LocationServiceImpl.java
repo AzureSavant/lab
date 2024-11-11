2b76513893409887d1ef6de7ca5a9a5e7f5e34df
AzureSavant <aleksandar.nonevski@students.finki.ukim.mk>
2024-11-11 09:31:25 +0100

    Add Location Service and Implementation

---
+++ src/main/java/mk/finki/ukim/mk/lab/service/impl/LocationServiceImpl.java
@@ -0,0 +1,30 @@
+package mk.finki.ukim.mk.lab.service.impl;
+
+import mk.finki.ukim.mk.lab.model.Location;
+import mk.finki.ukim.mk.lab.repository.LocationRepository;
+import mk.finki.ukim.mk.lab.service.ILocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LocationServiceImpl implements ILocationService {
+    @Autowired
+    private final LocationRepository locationRepository;
+
+    public LocationServiceImpl(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    @Override
+    public List<Location> findAll() {
+        return locationRepository.findAll();
+    }
+
+    @Override
+    public Location getLocationById(int id) {
+        return locationRepository.getLocationById(id);
+    }
+
+}
